f4de5aa8e9f2993bfca1930812ced33491045211
hyjkeew <hyjkeew@gmail.com>
2018-05-01 16:15:49 +0900

    근영 이용후기 완료

--- medi/src/kh/com/medi/service/impl/MediHbbsServiceImpl.java
+++ medi/src/kh/com/medi/service/impl/MediHbbsServiceImpl.java
@@ -9,6 +9,7 @@ import kh.com.medi.dao.MediHbbsDao;
 import kh.com.medi.model.MediDoctorDto;
 import kh.com.medi.model.MediDoctorSchedulDto;
 import kh.com.medi.model.MediHbbsParamDto;
+import kh.com.medi.model.MediHbbsReviews;
 import kh.com.medi.model.MediMember_hDto;
 import kh.com.medi.service.MediHbbsService;
 
@@ -42,6 +43,23 @@ public class MediHbbsServiceImpl implements MediHbbsService {
 	@Override
 	public List<MediMember_hDto> getHospitalList(MediHbbsParamDto hbbs) throws Exception {
 		return mediHbbsDao.getHospitalList(hbbs);
+	}
+
+	// 이용후기 시작
+	@Override
+	public boolean writeReviews(MediHbbsReviews redto) throws Exception {
+		return mediHbbsDao.writeReviews(redto);
+	}
+
+	@Override
+	public List<MediHbbsReviews> getReviews(MediHbbsParamDto hbbsp) throws Exception {
+		return mediHbbsDao.getReviews(hbbsp);
+	}
+
+	@Override
+	public int getReviewsCount(MediHbbsParamDto hbbsp) throws Exception {
+		// TODO Auto-generated method stub
+		return mediHbbsDao.getReviewsCount(hbbsp);
 	}
 
 }
